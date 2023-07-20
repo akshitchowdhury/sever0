9ed54e5ddf559807aaabd51d817817f2a34929e5
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-20 12:36:07 +0530

    Add files via upload

--- Server_requests/index.js
+++ Server_requests/index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+import express from 'express'
+
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log("Sever0 launched on 3000")
+})
+
+app.get("/", (req,res)=>{
+
+    res.send("welcome to Server0")
+})
+
