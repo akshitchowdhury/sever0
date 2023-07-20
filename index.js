08e44f8a6c2c2af56c41c2a201ccf20abfd71686
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-20 12:45:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log("Sever0 launched on 3000")
+})
+
+app.get("/", (req,res)=>{
+
+    res.send("<h1>Nodemon mode</h1>")
+})
+
